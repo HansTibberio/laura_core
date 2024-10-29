1be3dc77fd23f98928845d8db28083b484cddff9
HansTibberio <hanstiberio@proton.me>
2024-10-29 17:16:01 -0600

    Default implementation of prng

--- src/gen/random.rs
+++ src/gen/random.rs
@@ -6,6 +6,7 @@
 ///
 /// The implementation relies on bitwise operations and shifts to evolve the internal
 /// state and generate pseudorandom outputs.
+#[derive(Clone, Copy, Debug)]
 pub struct Xoshiro256PlusPlus {
     state: [u64; 4],
 }
@@ -15,6 +16,12 @@ impl Xoshiro256PlusPlus {
     /// Initializes a new instance of `Xoshiro256PlusPlus` with a given seed.
     pub fn new(seed: [u64; 4]) -> Self {
         Self { state: seed }
+    }
+
+    /// Initializes a new instance of `Xoshiro256PlusPlus` with a default seed.
+    /// The seed is the first 64 bits of the decimal part of the square roots of the first prime numbers in the interval `[2, 7]`.
+    pub fn default() -> Self {
+        Self { state: [0x6a09e667f3bcc908, 0xbb67ae8584caa73b, 0x3c6ef372fe94f82b, 0xa54ff53a5f1d36f1] }
     }
 
     /// Rotates the bits of a 64-bit unsigned integer `x` to the left by `k` positions.
@@ -47,11 +54,20 @@ impl Xoshiro256PlusPlus {
 }   
 
 #[test]
-fn test_prng() {
+fn test_prng_seed() {
     let seed: [u64; 4] = [0x1a2b3c4d5e6f7, 0x1122334455667788, 0x99aabbccddeeff00, 0x2233445566778899];
-    let mut rng: Xoshiro256PlusPlus = Xoshiro256PlusPlus::new(seed);
+    let mut prng: Xoshiro256PlusPlus = Xoshiro256PlusPlus::new(seed);
 
     for _ in 0..10 {
-        println!("{}", rng.next());
+        println!("{}", prng.next());
+    }
+}
+
+#[test]
+fn test_prng_default(){
+    let mut prng: Xoshiro256PlusPlus = Xoshiro256PlusPlus::default();
+
+    for _ in 0..10 {
+        println!("{}", prng.next());
     }
 }
